eb77faf3d3bdc6e68da0e103186645c9d4c157af
97arkham <khairunarkham97@gmail.com>
2022-06-21 11:35:18 +0700

    20220621

---
+++ Belajar Dasar Pemrograman Web/javascript/forloop.js
@@ -0,0 +1,26 @@
+// sederhana
+for(let i = 0; i<5;i++){
+    console.log(i);
+}
+
+/*output
+0
+1
+2
+3
+4
+
+*/
+
+//naik level forloop
+const nama = ["konto","lodon","kurnia","tod"];
+for( let i = 0; i < nama.length;i++){
+    console.log(nama[i]);
+}
+
+/*
+konto
+lodon
+kurnia
+tod
+*/
---
+++ Belajar Dasar Pemrograman Web/javascript/forofloop.js
@@ -0,0 +1,12 @@
+let nama = ["konto","lodon","kurnia","tod"];
+
+for(const namaOrang of nama){
+    console.log(namaOrang);
+}
+
+/*
+konto
+lodon
+kurnia
+tod
+*/
